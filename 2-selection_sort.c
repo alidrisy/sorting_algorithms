5ebdfa14815fadd06f812836dd0f350d7cf673e6
alidrisy <a.alidrisy6@gmail.com>
2023-07-21 21:21:00 +0300

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+* selection_sort -  sorts an array of integers in ascending order
+* using the Selection sort algorithm.
+* @array: an array of element to be sort
+* @size: size of array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	int x, i, n, j;
+
+	for (i = 0; i < size; i++)
+	{
+		n = i;
+		for (x = i + 1; x < size; x++)
+		{
+			if (array[x] < array[n])
+				n = x;
+		}
+		if (n != i)
+		{
+			j = array[i];
+			array[i] = array[n];
+			array[n] = j;
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -22,5 +22,8 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
