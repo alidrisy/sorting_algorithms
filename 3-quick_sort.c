15822161002ce0f4a7c584a8a9f3f1093c991525
alidrisy <a.alidrisy6@gmail.com>
2023-07-22 21:38:27 +0300

    fix errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,43 +1,59 @@
 #include "sort.h"
-#include <math.h>
+
+void quicksort(int *array, int lo, int hi, int size);
+
+/**
+* quick_sort -  sorts an array of integers in ascending order
+* using the Quick sort algorithm.
+* @array: an array of element to be sort
+* @size: size of array
+*/
+
+void quick_sort(int *array, size_t size)
+{
+if (!array || size < 2)
+return;
+
+quicksort(array, 0, size - 1, size);
+}
 
 /**
 * quick_partition -  sorts an array of integers in ascending order
 * using the Quick sort algorithm.
 * @array: an array of element to be sort
+* @size: size of array
 * @lo: low number of value
 * @hi: hight number of value
 * Return: the index of the hight value
 */
 
-int quick_partition(int *array, int lo, int hi)
+int quick_partition(int *array, int lo, int hi, int size)
 {
-int a = truncf(((hi - lo) / 2) + lo);
-int piv = array [a];
-int i = lo - 1;
+int piv = array[hi];
+int i = lo;
 int s, j;
 
-j = hi + 1;
 s = 0;
-while(1)
-{
-do
-{
-i += 1;
-}
-while (array[i] < piv);
-do
-{
-j -= 1;
-}
-while (array[j] > piv);
-if (i >= j)
-return j;
 
+for (j = lo; j < hi; j++)
+{
+if (array[j] <= piv)
+{
 s = array[i];
 array[i] = array[j];
 array[j] = s;
+if (s != array[i])
+print_array(array, size);
+i++;
 }
+}
+s = array[i];
+array[i] = array[hi];
+array[hi] = s;
+if (s != array[i])
+print_array(array, size);
+
+return (i);
 }
 
 /**
@@ -53,25 +69,11 @@ void quicksort(int *array, int lo, int hi, int size)
 {
 int j;
 
-if (lo >= 0 && hi >= 0 && lo < hi)
+if (lo < hi)
 {
-j = quick_partition(array, lo, hi);
-print_array(array, size);
-quicksort(array, lo, j, size);
+j = quick_partition(array, lo, hi, size);
+quicksort(array, lo, j - 1, size);
 quicksort(array, j + 1, hi, size);
 }
 }
 
-/**
-* quick_sort -  sorts an array of integers in ascending order
-* using the Quick sort algorithm.
-* @array: an array of element to be sort
-* @size: size of array
-*/
-
-void quick_sort(int *array, size_t size)
-{
-if (!array || size < 2)
-return;
-quicksort(array, 0, size - 1, size);
-}
