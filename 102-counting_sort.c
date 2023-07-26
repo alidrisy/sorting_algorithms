f91cbc1ed7165ad52ea444ca97f551e21f61300a
alidrisy <a.alidrisy6@gmail.com>
2023-07-26 17:46:21 +0300

    fix betty

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -22,7 +22,6 @@ for (i = 0; i < s; i++)
 if (array[i] > v)
 v = array[i];
 }
-
 count = malloc(sizeof(int) * (v + 1));
 for (i = 0; i <= v; i++)
 count[i] = 0;
@@ -32,22 +31,17 @@ for (i = 0; i < s; i++)
 x = array[i];
 count[x] = count[x] + 1;
 }
-
 for (i = 1; i <= v; i++)
 count[i] += count[i - 1];
-
 print_array(count, v + 1);
-
 for (i = s - 1; i >= 0; i--)
 {
 x = array[i];
 count[x] = count[x] - 1;
 output[count[x]] = array[i];
 }
-
 for (i = 0; i < s; i++)
 array[i] = output[i];
-
 free(output);
 free(count);
 }
